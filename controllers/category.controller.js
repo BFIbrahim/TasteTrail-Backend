4812049846316325eae75af35a28b6ac8e473eda
Md Ibrahim <mdibrahim36194@gmail.com>
2026-02-10 15:47:48 +0600

    category delete controller added

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -23,3 +23,20 @@ exports.getAllCategories = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    await Category.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { addCategory, getAllCategories } = require("../controllers/category.controller");
+const { addCategory, getAllCategories, deleteCategory } = require("../controllers/category.controller");
 const verifyToken = require("../middlewares/verifyToken");
 
 router.post("/", verifyToken, addCategory);
 router.get("/", verifyToken, getAllCategories);
+router.delete("/:id", verifyToken, deleteCategory);
 
 module.exports = router;
